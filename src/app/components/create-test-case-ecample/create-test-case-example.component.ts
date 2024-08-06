a3a6ee58e79b6645a642ca6f2363056859b032ba
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2024-08-06 23:02:08 +0400

    начал делать компонент создания тест-кейса

--- src/app/components/create-test-case-ecample/create-test-case-example.component.ts
+++ src/app/components/create-test-case-ecample/create-test-case-example.component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, signal, ViewChild} from '@angular/core';
 import {
   TestCase,
   TestCaseData,
@@ -13,6 +13,8 @@ import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatCheckbox} from "@angular/material/checkbox";
 import {FlexModule} from "@angular/flex-layout";
 import {MatIcon} from "@angular/material/icon";
+import {MatSidenav, MatSidenavContainer, MatSidenavModule} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
 
 @Component({
   selector: 'app-create-test-case-ecample',
@@ -24,14 +26,23 @@ import {MatIcon} from "@angular/material/icon";
     MatCheckbox,
     FlexModule,
     MatIconButton,
-    MatIcon
+    MatIcon,
+    MatSidenavContainer,
+    MatNavList,
+    MatListItem,
+    MatSidenavModule,
   ],
   templateUrl: './create-test-case-example.component.html',
   styleUrl: './create-test-case-example.component.css'
 })
 export class CreateTestCaseExampleComponent {
-  allSelected = false;
-  indeterminate = false;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  allSelectedSteps = false;
+  allSelectedPreConditions = false;
+  allSelectedPostConditions = false;
+  indeterminateSteps = false;
+  indeterminatePreCondition = false;
+  indeterminatePostCondition = false;
 
   private user: User = {
     id: 1,
@@ -95,6 +106,10 @@ export class CreateTestCaseExampleComponent {
   }
 
 
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
+
 addStep(){
     const step: TestCaseStep = {
       id: this.steps.length +1,
@@ -103,45 +118,46 @@ addStep(){
       expectedResult: ''
     }
     this.steps.push(step);
-    this.updateAllSelected();
+    this.updateAllSelectedSteps();
 }
 
   addPreCondition(){
     const preCondition: TestCasePreCondition = {
-      id: this.steps.length +1,
+      id: this.preconditions.length +1,
       selected: false,
       action: '',
       expectedResult: ''
     }
-    this.steps.push(preCondition);
-    this.updateAllSelected();
+    this.preconditions.push(preCondition);
+    this.updateAllSelectedPreconditions();
   }
 
   addPostCondition(){
     const postCondition: TestCasePostCondition = {
-      id: this.steps.length +1,
+      id: this.postConditions.length +1,
       selected: false,
       action: '',
       expectedResult: ''
     }
-    this.steps.push(postCondition);
-    this.updateAllSelected();
+    this.postConditions.push(postCondition);
+    this.updateAllSelectedPostConditions();
   }
 
   deleteSelectedSteps() {
     this.steps = this.steps.filter(step => !step.selected);
-    this.updateAllSelected();
+    this.updateAllSelectedSteps();
+    this.allSelectedSteps = false;
   }
 
   selectAllSteps(checked: boolean) {
     this.steps.forEach(step => step.selected = checked);
-    this.allSelected = checked;
+    this.allSelectedSteps = checked;
   }
 
-  updateAllSelected() {
+  updateAllSelectedSteps() {
     const totalSelected = this.steps.filter(step => step.selected).length;
-    this.allSelected = totalSelected === this.steps.length;
-    this.indeterminate = totalSelected > 0 && totalSelected < this.steps.length;
+    this.allSelectedSteps = totalSelected === this.steps.length;
+    this.indeterminateSteps = totalSelected > 0 && totalSelected < this.steps.length;
   }
 
   autoResize(event: Event) {
@@ -150,4 +166,37 @@ addStep(){
     target.style.height = target.scrollHeight + 'px';
   }
 
+  selectAllPreconditions(checked: boolean) {
+    this.preconditions.forEach(step => step.selected = checked);
+    this.allSelectedPreConditions = checked;
+  }
+
+  updateAllSelectedPreconditions() {
+    const totalSelected = this.preconditions.filter(step => step.selected).length;
+    this.allSelectedPreConditions = totalSelected === this.preconditions.length;
+    this.indeterminatePreCondition = totalSelected > 0 && totalSelected < this.preconditions.length;
+  }
+
+  deleteSelectedPreconditions() {
+    this.preconditions = this.preconditions.filter(step => !step.selected);
+    this.updateAllSelectedPreconditions();
+    this.allSelectedPreConditions = false;
+  }
+
+  selectAllPostConditions(checked: boolean) {
+    this.postConditions.forEach(step => step.selected = checked);
+    this.allSelectedPostConditions = checked;
+  }
+
+  updateAllSelectedPostConditions() {
+    const totalSelected = this.steps.filter(step => step.selected).length;
+    this.allSelectedPostConditions = totalSelected === this.postConditions.length;
+    this.indeterminatePostCondition = totalSelected > 0 && totalSelected < this.postConditions.length;
+  }
+
+  deleteSelectedPostConditions() {
+    this.postConditions = this.postConditions.filter(step => !step.selected);
+    this.updateAllSelectedPostConditions();
+    this.allSelectedPostConditions = false;
+  }
 }
