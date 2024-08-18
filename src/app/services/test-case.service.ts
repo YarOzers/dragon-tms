3fffa4b0b3e41f58faccbd1141fdd2dd9ae22572
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2024-08-19 01:54:13 +0400

    добавил методы в testCaseService

--- src/app/services/test-case.service.ts
+++ src/app/services/test-case.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {environment} from "../environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {TestCase} from "../models/test-case";
+import {TestCase, TestCaseData} from "../models/test-case";
 import {Folder} from "../models/folder";
 
 @Injectable({
@@ -18,5 +18,25 @@ export class TestCaseService {
     console.log(`Get testCases in folder ${folderId} : ${testCases}`);
 
     return testCases;
+  }
+
+  addTestCaseToFolder(folderId: number, testCase: TestCase):Observable<TestCase>{
+    return this.http.post<TestCase>(`${this.apiUrl}/testcases/folder/${folderId}`,testCase)
+  }
+
+  updateTestCase(testCaseId: number, testCaseDate: TestCaseData): Observable<TestCase>{
+    return this.http.put<TestCase>(`${this.apiUrl}/testcases/${testCaseId}`,testCaseDate);
+  }
+
+  moveTestCase(testCaseId: number, targetFolderId: number):Observable<TestCase>{
+    return this.http.put<TestCase>(`${this.apiUrl}/${testCaseId}/move/${targetFolderId}`, null);
+  }
+
+  copyTestCase(testCaseId: number, targetFolderId: number): Observable<TestCase>{
+    return this.http.post<TestCase>(`${this.apiUrl}/${testCaseId}/copy/${targetFolderId}`,null);
+  }
+
+  deleteTestCase(testCaseId:number): Observable<void>{
+    return this.http.delete<void>(`${this.apiUrl}/${testCaseId}`);
   }
 }
