b8776b7f5a55d1311b999dca2550c2802ab8078a
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2024-09-13 23:28:31 +0400

    перед удалением старого редактора

--- src/app/components/ckeditor/ck-editor/ck-editor.component.ts
+++ src/app/components/ckeditor/ck-editor/ck-editor.component.ts
@@ -14,16 +14,45 @@ import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
 
 import {
   AccessibilityHelp,
-  AutoLink,
+  Autoformat,
+  AutoImage,
   Autosave,
   BalloonEditor,
+  BalloonToolbar,
+  Base64UploadAdapter,
+  BlockQuote,
+  BlockToolbar,
   Bold,
+  Code,
   type EditorConfig,
   Essentials,
+  Heading,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsert,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
   Italic,
   Link,
+  LinkImage,
+  List,
   Paragraph,
+  PasteFromOffice,
   SelectAll,
+  Strikethrough,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
+  Underline,
   Undo
 } from 'ckeditor5';
 import {FormsModule} from "@angular/forms";
@@ -58,11 +87,122 @@ export class CkEditorComponent implements AfterViewInit, OnChanges{
   public ngAfterViewInit(): void {
     this.config = {
       toolbar: {
-        items: ['undo', 'redo', '|', 'bold', 'italic', '|', 'link'],
+        items: [
+          'undo',
+          'redo',
+          '|',
+          'heading',
+          '|',
+          'bold',
+          'italic',
+          'underline',
+          'strikethrough',
+          'code',
+          '|',
+          'link',
+          'insertImage',
+          'insertTable',
+          'blockQuote',
+          '|',
+          'bulletedList',
+          'numberedList',
+          'todoList'
+        ],
         shouldNotGroupWhenFull: false
       },
-      plugins: [AccessibilityHelp, AutoLink, Autosave, Bold, Essentials, Italic, Link, Paragraph, SelectAll, Undo],
-      initialData: '',
+      plugins: [
+        AccessibilityHelp,
+        Autoformat,
+        AutoImage,
+        Autosave,
+        BalloonToolbar,
+        Base64UploadAdapter,
+        BlockQuote,
+        BlockToolbar,
+        Bold,
+        Code,
+        Essentials,
+        Heading,
+        ImageBlock,
+        ImageCaption,
+        ImageInline,
+        ImageInsert,
+        ImageInsertViaUrl,
+        ImageResize,
+        ImageTextAlternative,
+        ImageToolbar,
+        ImageUpload,
+        Italic,
+        Link,
+        LinkImage,
+        List,
+        Paragraph,
+        PasteFromOffice,
+        SelectAll,
+        Strikethrough,
+        Table,
+        TableCaption,
+        TableCellProperties,
+        TableColumnResize,
+        TableProperties,
+        TableToolbar,
+        TextTransformation,
+        TodoList,
+        Underline,
+        Undo
+      ],
+      balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
+      blockToolbar: ['bold', 'italic', '|', 'link', 'insertImage', 'insertTable', '|', 'bulletedList', 'numberedList'],
+      heading: {
+        options: [
+          {
+            model: 'paragraph',
+            title: 'Paragraph',
+            class: 'ck-heading_paragraph'
+          },
+          {
+            model: 'heading1',
+            view: 'h1',
+            title: 'Heading 1',
+            class: 'ck-heading_heading1'
+          },
+          {
+            model: 'heading2',
+            view: 'h2',
+            title: 'Heading 2',
+            class: 'ck-heading_heading2'
+          },
+          {
+            model: 'heading3',
+            view: 'h3',
+            title: 'Heading 3',
+            class: 'ck-heading_heading3'
+          },
+          {
+            model: 'heading4',
+            view: 'h4',
+            title: 'Heading 4',
+            class: 'ck-heading_heading4'
+          },
+          {
+            model: 'heading5',
+            view: 'h5',
+            title: 'Heading 5',
+            class: 'ck-heading_heading5'
+          },
+          {
+            model: 'heading6',
+            view: 'h6',
+            title: 'Heading 6',
+            class: 'ck-heading_heading6'
+          }
+        ]
+      },
+      image: {
+        toolbar: ['toggleImageCaption', 'imageTextAlternative', '|', 'resizeImage']
+      },
+      initialData:
+        '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
       link: {
         addTargetToExternalLinks: true,
         defaultProtocol: 'https://',
@@ -76,7 +216,10 @@ export class CkEditorComponent implements AfterViewInit, OnChanges{
           }
         }
       },
-      placeholder: ''
+      placeholder: 'Type or paste your content here!',
+      table: {
+        contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+      }
     };
 
     this.isLayoutReady = true;
